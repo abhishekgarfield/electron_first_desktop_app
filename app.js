66f42daecb9d35d4708a333bedf2761eda440fe2
abhishekgarfield <abhishek2759@gmail.com>
2023-04-22 20:41:15 +0530

    finish

---
+++ app.js
@@ -0,0 +1,12 @@
+import { ReactDOM } from "react"
+const root = document.getElementById('root')
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Electron!</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, root)
